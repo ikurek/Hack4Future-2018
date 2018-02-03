448dd8e2392a4a6340efcfc0429b594bd797fea9
Igor Kurek <igorkurek96@gmail.com>
2018-02-03 08:40:29 +0100

    xddddd

--- app/src/main/java/com/lostapiseekers/fridgeapp/MainActivity.java
+++ app/src/main/java/com/lostapiseekers/fridgeapp/MainActivity.java
@@ -1,14 +1,11 @@
 package com.lostapiseekers.fridgeapp;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.util.SparseBooleanArray;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -19,23 +16,11 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.ArrayList;
-
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends Activity {
 
     private DatabaseReference databaseReference;
-
     private FirebaseAuth firebaseAuth;
 
-    private Button buttonAddItem;
-    private Button buttonDelete;
-    private Button buttonSaveList;
-
-    private ArrayList<String> itemList;
-    private ArrayAdapter<String> adapter;
-
-    private EditText itemText;
-    private ListView listView;
 
 
     @Override
@@ -52,85 +37,44 @@ public class MainActivity extends Activity implements View.OnClickListener {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
-                            Toast.makeText(this, "Login successful", Toast.LENGTH_LONG).show();
                             FirebaseUser user = firebaseAuth.getCurrentUser();
                         } else {
                             // If sign in fails, display a message to the user.
 
-                            Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show();
-                            FirebaseUser user = null ;
+                            Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = null;
                         }
                     }
                 });
 
-
-        listView = (ListView) findViewById(R.id.listView);
-        itemText = (EditText) findViewById(R.id.editText);
-        itemList = new ArrayList<>();
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, itemList);
-
-        View.OnClickListener addListener = new View.OnClickListener() {
+        findViewById(R.id.add_button).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                itemList.add(itemText.getText().toString().trim());
-                itemText.setText("");
-                adapter.notifyDataSetChanged();
+            public void onClick(View v) {
+                switchFragment(new ScanFragment(false), false);
             }
-        };
+        });
 
-        View.OnClickListener deleteItem = new View.OnClickListener() {
+        findViewById(R.id.show_button).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                SparseBooleanArray positionChecker = listView.getCheckedItemPositions();
-                int count = listView.getCount();
-                for (int item = count - 1; count == 0; count--) {
-                    if (positionChecker.get(item)) {
-                        adapter.remove(itemList.get(item));
-                    }
-                }
-                positionChecker.clear();
-                adapter.notifyDataSetChanged();
+            public void onClick(View v) {
+                switchFragment(new ProductsFragment(), false);
             }
-        };
+        });
 
-        buttonAddItem = (Button) findViewById(R.id.buttonAddItem);
-        buttonDelete = (Button) findViewById(R.id.buttonDelete);
-        buttonSaveList = (Button) findViewById(R.id.buttonSaveList);
-
-
-        buttonSaveList.setOnClickListener(this);
-        buttonAddItem.setOnClickListener(addListener);
-        buttonDelete.setOnClickListener(deleteItem);
-
-        listView.setAdapter(adapter);
-
+        switchFragment(new ProductsFragment(), false);
     }
 
-    private void saveUserList() {
-        UserList userList = new UserList(itemList);
-        userList.itemList = getList();
+    private void switchFragment(Fragment fragment, Boolean backstack) {
 
-        FirebaseUser user = firebaseAuth.getCurrentUser();
+        // Change fragment fo selected one
+        FragmentTransaction transaction = getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_frame, fragment);
 
-        databaseReference.child(user.getUid()).setValue(userList);
-        Toast.makeText(this, "List Saved", Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    public void onClick(View view) {
-        if (view == buttonSaveList) {
-            saveUserList();
+        if (backstack) {
+            transaction.addToBackStack(null);
         }
 
-    }
-
-    public ArrayList getList() {
-        return itemList;
-    }
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        transaction.commit();
     }
 }
--- app/src/main/java/com/lostapiseekers/fridgeapp/userList.java
+++
@@ -1,8 +0,0 @@
-package com.lostapiseekers.fridgeapp;
-
-/**
- * Created by michal on 02.02.18.
- */
-
-public class userList {
-}
